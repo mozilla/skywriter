5fe0d17ac3d45b4695ac2633a9ac61bacb7822cf
Kevin Dangoor <kdangoor@mozilla.com>
2009-09-22 15:52:18 -0400

    initial commit - making the bootstrap and pavement a little nicer than the old versions (moving docs from README into the output of the script)

---
+++ pavement.py
@@ -0,0 +1,105 @@
+#  ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1
+# 
+# The contents of this file are subject to the Mozilla Public License  
+# Version
+# 1.1 (the "License"); you may not use this file except in compliance  
+# with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+# 
+# Software distributed under the License is distributed on an "AS IS"  
+# basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the  
+# License
+# for the specific language governing rights and limitations under the
+# License.
+# 
+# The Original Code is Bespin.
+# 
+# The Initial Developer of the Original Code is Mozilla.
+# Portions created by the Initial Developer are Copyright (C) 2009
+# the Initial Developer. All Rights Reserved.
+# 
+# Contributor(s):
+# 
+# ***** END LICENSE BLOCK *****
+# 
+import sys
+
+from paver.easy import *
+import paver.virtual
+
+options(
+    version=Bunch(
+        number="0.6",
+        name="Reboot1",
+        api="4"
+    ),
+    virtualenv=Bunch(
+        paver_command_line="initial"
+    )
+)
+
+@task
+def initial():
+    """Initial setup help."""
+    venv_command = "Scripts/activate.bat" if sys.platform == 'win32' \
+        else "source bin/activate"
+    linux_note = "" if not sys.platform.startswith("linux") else """
+
+    *** NOTE FOR LINUX USERS: If you are running on a Linux system, you will 
+    likely need a "python-dev" (on Ubuntu, possibly python-devel elsewhere) 
+    package installed and either the libc-dev or libc6-dev package, if you 
+    do not already have them.
+
+"""
+    
+    mac_note = "" if not sys.platform.startswith("darwin") else """
+
+    *** NOTE FOR MAC USERS: You will need Xcode installed.
+    
+    If you put your Bespin checkout in a directory
+    that has a space in the directory name, the bootstrap script will fail
+    with an error "no such file or directory" because there will be
+    scripts generated with an invalid shebang (#!) line.
+
+"""
+
+    win_note = "" if not sys.platform.startswith("win") else """
+
+    Note about running on Windows
+    -----------------------------
+
+    The current, up-to-date Bespin backend is written in Python. Because
+    Python is cross-platform, it should be possible (and likely not too
+    difficult) to make the backend work on Windows once Python 2.5 is
+    installed. However, this has not been tested and there are likely two
+    issues:
+
+    1. some libraries used by Bespin try to compile C code (*)
+    2. some paths may not be correct on Windows systems
+
+    Microsoft offers free command line compilers that work well with
+    Python.
+
+"""
+    print """Welcome to the Bespin Developer Build!
+
+We want to get your development server up and running quickly.
+If you run into trouble, please head over to the Bespin
+Googlegroup and we can help:
+
+http://groups.google.com/group/bespin/
+
+We maintain a Python-based web server that is easy to get
+running in development. It works with Python 2.5 or 2.6
+only.
+%s%s%s
+The Python server lives in a separate Mercurial repository.
+To get the Python server installed, run these two commands:
+
+%s
+paver install_server
+""" % (mac_note, linux_note, win_note, venv_command)
+
