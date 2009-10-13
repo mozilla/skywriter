cb544ceb8c253b5d6d2f54ba585afc2e8f03a507
Kevin Dangoor <kdangoor@mozilla.com>
2009-10-13 11:55:42 -0400

    testing out boot changes based on recent Narwhal updates
    --HG--
    extra : rebase_source : f636de3489252756855a722f081001ea171658ea

--- src/bespin-core/lib/bespin/boot.js
+++ src/bespin-core/lib/bespin/boot.js
@@ -22,6 +22,11 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
+// This module is set up to be dependency-less so that Narwhal
+// will be able to run it right away.
+
+console.log("Made it into boot!");
+
 // TODO "prequire" is a hack and should be replaced when
 // require.when/require.async become available for real.
 require.prequire.when("bespin/embed", function() {
