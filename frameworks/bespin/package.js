83c47084cf80f8c1305f860c622fea0a08515423
Kevin Dangoor <kdangoor@mozilla.com>
2009-11-06 14:14:42 -0500

    rename bespin-core to bespin, which will make things more consistent with how requires() had been previously.
    --HG--
    rename : src/bespin-core/lib/bespin/actions.js => frameworks/bespin/actions.js
    rename : src/bespin-core/lib/bespin/boot.js => frameworks/bespin/boot.js
    rename : src/bespin-core/lib/bespin/builtins.js => frameworks/bespin/builtins.js
    rename : src/bespin-core/lib/bespin/builtins.json => frameworks/bespin/builtins.json
    rename : src/bespin-core/lib/bespin/command.js => frameworks/bespin/command.js
    rename : src/bespin-core/lib/bespin/cursor.js => frameworks/bespin/cursor.js
    rename : src/bespin-core/lib/bespin/editor.js => frameworks/bespin/editor.js
    rename : src/bespin-core/lib/bespin/editor/controller.js => frameworks/bespin/editor/controller.js
    rename : src/bespin-core/lib/bespin/editor/views/editor.js => frameworks/bespin/editor/views/editor.js
    rename : src/bespin-core/lib/bespin/editor/views/scroller.js => frameworks/bespin/editor/views/scroller.js
    rename : src/bespin-core/lib/bespin/embed.js => frameworks/bespin/embed.js
    rename : src/bespin-core/lib/bespin/events.js => frameworks/bespin/events.js
    rename : src/bespin-core/lib/bespin/history.js => frameworks/bespin/history.js
    rename : src/bespin-core/lib/bespin/model.js => frameworks/bespin/model.js
    rename : src/bespin-core/lib/bespin.js => frameworks/bespin/package.js
    rename : src/bespin-core/lib/bespin/plugins.js => frameworks/bespin/plugins.js
    rename : src/bespin-core/lib/bespin/settings.js => frameworks/bespin/settings.js
    rename : src/bespin-core/lib/bespin/syntax.js => frameworks/bespin/syntax.js
    rename : src/bespin-core/lib/bespin/tests/allTests.js => frameworks/bespin/tests/allTests.js
    rename : src/bespin-core/lib/bespin/tests/bootTests.js => frameworks/bespin/tests/bootTests.js
    rename : src/bespin-core/lib/bespin/tests/cmdline.js => frameworks/bespin/tests/cmdline.js
    rename : src/bespin-core/lib/bespin/theme.js => frameworks/bespin/theme.js
    rename : src/bespin-core/lib/bespin/util/bootstrap_worker.js => frameworks/bespin/util/bootstrap_worker.js
    rename : src/bespin-core/lib/bespin/util/canvas.js => frameworks/bespin/util/canvas.js
    rename : src/bespin-core/lib/bespin/util/clipboard.js => frameworks/bespin/util/clipboard.js
    rename : src/bespin-core/lib/bespin/util/container.js => frameworks/bespin/util/container.js
    rename : src/bespin-core/lib/bespin/util/cookie.js => frameworks/bespin/util/cookie.js
    rename : src/bespin-core/lib/bespin/util/globals.js => frameworks/bespin/util/globals.js
    rename : src/bespin-core/lib/bespin/util/hub.js => frameworks/bespin/util/hub.js
    rename : src/bespin-core/lib/bespin/util/keys.js => frameworks/bespin/util/keys.js
    rename : src/bespin-core/lib/bespin/util/mousewheelevent.js => frameworks/bespin/util/mousewheelevent.js
    rename : src/bespin-core/lib/bespin/util/path.js => frameworks/bespin/util/path.js
    rename : src/bespin-core/lib/bespin/util/tokenobject.js => frameworks/bespin/util/tokenobject.js
    rename : src/bespin-core/lib/bespin/util/util.js => frameworks/bespin/util/util.js
    extra : rebase_source : 0f4fe228f4aee06a7270e4e2e80f3bc7eda01ec1

---
+++ frameworks/bespin/package.js
@@ -0,0 +1,66 @@
+
+* /* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
+ * See the License for the specific language governing rights and
+ * limitations under the License.
+ *
+ * The Original Code is Bespin.
+ *
+ * The Initial Developer of the Original Code is Mozilla.
+ * Portions created by the Initial Developer are Copyright (C) 2009
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Bespin Team (bespin@mozilla.com)
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+var SC = require("sproutcore");
+
+// Version numbers
+SC.mixin(exports, {
+    // BEGIN VERSION BLOCK
+    /** The core version of the Bespin system */
+    versionNumber: 'tip',
+    /** The version number to display to users */
+    versionCodename: 'DEVELOPMENT MODE',
+    /** The version number of the API (to ensure that the client and server are talking the same language) */
+    apiVersion: 'dev',
+    // END VERSION BLOCK
+
+    /** The name of the project that contains the users client side settings */
+    userSettingsProject: "BespinSettings"
+});
+
+/**
+ * Set innerHTML on the element given, with the Bespin version info
+ */
+exports.displayVersion = function(el) {
+    el = document.getElementById(el) || document.getElementById("version");
+    if (!el) {
+        return;
+    }
+    el.innerHTML = '<a href="https://wiki.mozilla.org/Labs/Bespin/ReleaseNotes" title="Read the release notes">Version <span class="versionnumber">' + this.versionNumber + '</span> "' + this.versionCodename + '"</a>';
+};
+
+// Clone the hub
+var hub = require("bespin/util/hub");
+exports.publish = hub.publish;
+exports.subscribe = hub.subscribe;
+exports.unsubscribe = hub.unsubscribe;
+exports.fireAfter = hub.fireAfter;
+
+// Clone the container
+var container = require("bespin/util/container");
+exports.register = container.register;
+exports.unregister = container.unregister;
+exports.get = container.get;
+exports.getComponent = container.getComponent;
