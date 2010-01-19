164a6cb38f57c74730df56e989cee054f85e9d54
Joe Walker <jwalker@mozilla.com>
2010-01-19 15:19:49 +0000

    moving wizard into labs
    --HG--
    rename : src/bespin-supported/lib/bespin/wizard.js => plugins/labs/Wizard.js

--- plugins/labs/Wizard.js
+++ plugins/labs/Wizard.js
@@ -1,29 +1,65 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.1 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
  *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied.
- * See the License for the specific language governing rights and
- * limitations under the License.
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
  * The Original Code is Bespin.
  *
- * The Initial Developer of the Original Code is Mozilla.
+ * The Initial Developer of the Original Code is
+ * Mozilla.
  * Portions created by the Initial Developer are Copyright (C) 2009
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
  *   Bespin Team (bespin@mozilla.com)
  *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
  * ***** END LICENSE BLOCK ***** */
 
-var webpieces = require("bespin/util/webpieces");
-var command = require("bespin/command");
+"define metadata";
+({
+    "provides": [
+        {
+            "ep": "command",
+            "name": "wizard",
+            "takes": [ "type" ],
+            "hidden": true,
+            "description": "display a named wizard to step through some process",
+            "completeText": "The name of the wizard to run. Leave blank to list known wizards",
+            "pointer": "#wizardCommand"
+        }
+    ]
+});
+"end";
+
+// TODO: re-implement this:
+var webpieces = {
+    showCenterPopup: function() {
+        throw "webpieces.showCenterPopup not implemented";
+    },
+    hideCenterPopup: function() {
+        throw "webpieces.showCenterPopup not implemented";
+    }
+};
 
 /**
  * This list of wizards that we can run. Each must have a url, which is a
@@ -112,26 +148,19 @@ exports.wizard = {
 };
 
 /**
- * The wizard command to show a wizard
+ * The 'wizard' command to show a wizard
  */
-command.store.addCommand({
-    "name": "wizard",
-    "takes": [ "type" ],
-    "hidden": true,
-    "description": "display a named wizard to step through some process",
-    "completeText": "The name of the wizard to run. Leave blank to list known wizards",
-    execute: function(instruction, type) {
-        if (!type) {
-            var list = "";
-            for (var name in wizards) {
-                if (wizards.hasOwnProperty(name)) {
-                    list += ", " + name;
-                }
+exports.wizardCommand = function(instruction, type) {
+    if (!type) {
+        var list = "";
+        for (var name in wizards) {
+            if (wizards.hasOwnProperty(name)) {
+                list += ", " + name;
             }
-            instruction.addOutput("Known wizards: " + list.substring(2));
-            return;
         }
-
-        exports.show(instruction, type, true);
+        instruction.addOutput("Known wizards: " + list.substring(2));
+        return;
     }
-});
+
+    exports.show(instruction, type, true);
+};
