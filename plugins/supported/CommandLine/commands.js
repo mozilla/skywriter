dd402c662ac48972cdc55c6f61e1bd33e91b2677
Kevin Dangoor <kdangoor@mozilla.com>
2010-02-02 12:10:35 -0500

    make the help command work

--- plugins/supported/CommandLine/commands.js
+++ plugins/supported/CommandLine/commands.js
@@ -36,15 +36,90 @@
  * ***** END LICENSE BLOCK ***** */
 
 var cliController = require("controller").cliController;
+var catalog = require("bespin:plugins").catalog;
 
 // TODO: fix
 var rootCanon = { aliases:[], commands:[] };
 
 /**
+ * Generate some help text for all commands in this canon, optionally
+ * filtered by a <code>prefix</code>, and with a <code>helpSuffix</code>
+ * appended.
+ */
+var _getHelp = function(prefix, options) {
+    var commands = [];
+    var command, name;
+    
+    command = catalog.getExtensionByKey("command", prefix);
+
+    if (command) { // caught a real command
+        commands.push(command.description);
+    } else {
+        var showHidden = false;
+
+        if (prefix) {
+            if (prefix == "hidden") { // sneaky, sneaky.
+                prefix = "";
+                showHidden = true;
+            }
+            commands.push("<h2>Commands starting with '" + prefix + "':</h2>");
+        } else {
+            commands.push("<h2>Available Commands:</h2>");
+        }
+
+        var toBeSorted = [];
+        catalog.getExtensions("command").forEach(function(command) {
+            toBeSorted.push(command.name);
+        });
+
+        var sorted = toBeSorted.sort();
+
+        commands.push("<table>");
+        for (var i = 0; i < sorted.length; i++) {
+            name = sorted[i];
+            command = catalog.getExtensionByKey("command", name);
+            if (!command) {
+                console.error("Huh? command ", name, " cannot be looked up by name");
+                continue;
+            }
+
+            if (!showHidden && command.hidden) {
+                continue;
+            }
+            if (command.description === undefined) {
+                // Ignore editor actions
+                continue;
+            }
+            if (prefix && name.indexOf(prefix) !== 0) {
+                continue;
+            }
+            
+            // todo add back a column with parameter information, perhaps?
+            
+            commands.push("<tr>");
+            commands.push('<th>' + name + '</th>');
+            commands.push('<td>' + command.description + "</td>");
+            commands.push("</tr>");
+        }
+        commands.push("</table>");
+    }
+
+    var output = commands.join("");
+    if (options && options.prefix) {
+        output = options.prefix + output;
+    }
+    if (options && options.suffix) {
+        output = output + options.suffix;
+    }
+    return output;
+};
+
+
+/**
  * TODO: make this automatic
  */
-exports.helpCommand = function(instruction, args) {
-    var output = this.parent.getHelp(args.search, {
+exports.helpCommand = function(instruction, args, request) {
+    var output = _getHelp(args.search, {
         prefix: "<h2>Welcome to Bespin - Code in the Cloud</h2><ul>" +
             "<li><a href='http://labs.mozilla.com/projects/bespin' target='_blank'>Home Page</a>" +
             "<li><a href='https://wiki.mozilla.org/Labs/Bespin' target='_blank'>Wiki</a>" +
@@ -55,7 +130,7 @@ exports.helpCommand = function(instruction, args) {
             "</ul>",
          suffix: "For more information, see the <a href='https://wiki.mozilla.org/Labs/Bespin'>Bespin Wiki</a>."
     });
-    instruction.addOutput(output);
+    request.done(output);
 };
 
 /**
