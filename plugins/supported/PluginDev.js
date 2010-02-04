bacea2d9f4428aefb94fdac9b69747d9ec38bde0
Kevin Dangoor <kdangoor@mozilla.com>
2010-02-04 09:29:50 -0500

    make tests work

--- plugins/supported/PluginDev.js
+++ plugins/supported/PluginDev.js
@@ -55,6 +55,10 @@
 });
 "end";
 
+// this isn't necessary under Abbot, and it's unclear to me why it's
+// necessary otherwise -- kdangoor
+require.loader.async("core_test");
+
 var core_test = require("core_test");
 var assert = require("core_test:assert");
 var test = require("core_test:test");
