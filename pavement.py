6ebaa6a8af088d7d8f7f810b7d5c75f3a23f26e0
Kevin Dangoor <kdangoor@mozilla.com>
2010-04-22 13:39:07 -0400

    set the version number to 0.7.3

--- pavement.py
+++ pavement.py
@@ -49,7 +49,7 @@ except ImportError:
 
 setup(
     name="dryice",
-    version="0.7.2",
+    version="0.7.3",
     packages=["dryice"],
     entry_points="""
 [console_scripts]
@@ -59,8 +59,8 @@ dryice=dryice.tool:main
 
 options(
     version=Bunch(
-        number="0.7.2",
-        name="Ashwell",
+        number="0.7.3",
+        name="Bryce",
         api="4"
     ),
     virtualenv=Bunch(
