52eb65c348c3e17945b759a3cea2adb1b6e7ecc2
Joe Walker <jwalker@mozilla.com>
2010-05-05 11:35:44 +0100

    de-SC the scratch canvas

--- frameworks/bespin/util/scratchcanvas.js
+++ frameworks/bespin/util/scratchcanvas.js
@@ -35,57 +35,52 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-var SC = require('sproutcore/runtime').SC;
 var util = require('bespin:util/util');
 
 /**
- * @class
- *
  * A invisible singleton canvas on the page, useful whenever a canvas context
  * is needed (e.g. for computing text sizes), but an actual canvas isn't handy
  * at the moment.
+ * @constructor
  */
-var ScratchCanvas = SC.Object.extend({
-    getContext: function() {
-        return document.getElementById('bespin-scratch-canvas').
-            getContext('2d');
-    },
+var ScratchCanvas = function() {
+    this._canvas = document.getElementById('bespin-scratch-canvas');
 
-    init: function() {
-        // It's possible that another ScratchCanvas instance in another sandbox
-        // exists on the page. If so, we assume they're compatible, and use
-        // that one.
-        if (!util.none(document.getElementById('bespin-scratch-canvas'))) {
-            return;
-        }
-
-        var element = document.createElement('canvas');
-        element.id = 'bespin-scratch-canvas';
-        element.width = 400;
-        element.height = 300;
-        element.style.position = 'absolute';
-        element.style.top = "-10000px";
-        element.style.left = "-10000px";
-        document.body.appendChild(element);
-    },
-
-    /**
-     * Returns the width in pixels of the given string ("M", by default) in the
-     * given font.
-     */
-    measureStringWidth: function(font, str) {
-        if (util.none(str)) {
-            str = "M";
-        }
-
-        var context = this.getContext();
-        context.save();
-        context.font = font;
-        var width = context.measureText(str).width;
-        context.restore();
-        return width;
+    // It's possible that another ScratchCanvas instance in another sandbox
+    // exists on the page. If so, we assume they're compatible, and use
+    // that one.
+    if (util.none(this._canvas)) {
+        this._canvas = document.createElement('canvas');
+        this._canvas.id = 'bespin-scratch-canvas';
+        this._canvas.width = 400;
+        this._canvas.height = 300;
+        this._canvas.style.position = 'absolute';
+        this._canvas.style.top = "-10000px";
+        this._canvas.style.left = "-10000px";
+        document.body.appendChild(this._canvas);
     }
-});
+};
+
+ScratchCanvas.prototype.getContext = function() {
+    return this._canvas.getContext('2d');
+};
+
+/**
+ * Returns the width in pixels of the given string ("M", by default) in the
+ * given font.
+ */
+ScratchCanvas.prototype.measureStringWidth = function(font, str) {
+    if (util.none(str)) {
+        str = "M";
+    }
+
+    var context = this.getContext();
+    context.save();
+    context.font = font;
+    var width = context.measureText(str).width;
+    context.restore();
+    return width;
+};
 
 var singleton = null;
 
@@ -95,8 +90,7 @@ var singleton = null;
  */
 exports.get = function() {
     if (singleton === null) {
-        singleton = ScratchCanvas.create();
+        singleton = new ScratchCanvas();
     }
     return singleton;
 }
-
