e8a8a5735b3efe44d4a5193e6ed538291a744d78
Kevin Dangoor <kdangoor@mozilla.com>
2010-06-04 15:40:05 -0400

    make the TextView use cursor: text again.
    --HG--
    extra : rebase_source : 446f35ba55757a2377b1435d6caf7e97bf66eedf

--- plugins/supported/text_editor/views/text.js
+++ plugins/supported/text_editor/views/text.js
@@ -38,6 +38,8 @@
 var catalog = require('bespin:plugins').catalog;
 var util = require('bespin:util/util');
 
+var $ = require("jquery").$;
+
 var Event = require('events').Event;
 var CanvasView = require('views/canvas').CanvasView;
 var LayoutManager = require('controllers/layoutmanager').LayoutManager;
@@ -73,6 +75,7 @@ exports.TextView = function(container, editor) {
     // 2) mouse events
     // etc...
     var dom = this.domNode;
+    $(dom).css("cursor", "text");
     dom.addEventListener('mousedown', this.mouseDown.bind(this), false);
     dom.addEventListener('mousemove', this.mouseMove.bind(this), false);
     window.addEventListener('mouseup', this.mouseUp.bind(this), false);
