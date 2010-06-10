7b66fd9367e7605cc1f6aae4c81f12a8b08275f4
Joe Walker <jwalker@mozilla.com>
2010-06-10 18:16:01 +0100

    minor tidyups
    --HG--
    extra : rebase_source : dca89bca4a7b89eccc5b6506da6e2fc96e3d3c02

--- plugins/boot/bespin/globals.js
+++ plugins/boot/bespin/globals.js
@@ -96,31 +96,35 @@ if (!Object.defineProperty) {
         var has = Object.prototype.hasOwnProperty;
         if (typeof descriptor == "object" && object.__defineGetter__) {
             if (has.call(descriptor, "value")) {
-                if (!object.__lookupGetter__(property) && !object.__lookupSetter__(property))
+                if (!object.__lookupGetter__(property) && !object.__lookupSetter__(property)) {
                     // data property defined and no pre-existing accessors
                     object[property] = descriptor.value;
-                if (has.call(descriptor, "get") || has.call(descriptor, "set"))
+                }
+                if (has.call(descriptor, "get") || has.call(descriptor, "set")) {
                     // descriptor has a value property but accessor already exists
                     throw new TypeError("Object doesn't support this action");
+                }
             }
             // fail silently if "writable", "enumerable", or "configurable"
             // are requested but not supported
             /*
-// alternate approach:
-if ( // can't implement these features; allow false but not true
-!(has.call(descriptor, "writable") ? descriptor.writable : true) ||
-!(has.call(descriptor, "enumerable") ? descriptor.enumerable : true) ||
-!(has.call(descriptor, "configurable") ? descriptor.configurable : true)
-)
-throw new RangeError(
-"This implementation of Object.defineProperty does not " +
-"support configurable, enumerable, or writable."
-);
-*/
-            else if (typeof descriptor.get == "function")
+            // alternate approach:
+            if ( // can't implement these features; allow false but not true
+            !(has.call(descriptor, "writable") ? descriptor.writable : true) ||
+            !(has.call(descriptor, "enumerable") ? descriptor.enumerable : true) ||
+            !(has.call(descriptor, "configurable") ? descriptor.configurable : true)
+            )
+            throw new RangeError(
+            "This implementation of Object.defineProperty does not " +
+            "support configurable, enumerable, or writable."
+            );
+            */
+            else if (typeof descriptor.get == "function") {
                 object.__defineGetter__(property, descriptor.get);
-            if (typeof descriptor.set == "function")
+            }
+            if (typeof descriptor.set == "function") {
                 object.__defineSetter__(property, descriptor.set);
+            }
         }
         return object;
     };
@@ -130,8 +134,9 @@ throw new RangeError(
 if (!Object.defineProperties) {
     Object.defineProperties = function(object, properties) {
         for (var property in properties) {
-            if (Object.prototype.hasOwnProperty.call(properties, property))
+            if (Object.prototype.hasOwnProperty.call(properties, property)) {
                 Object.defineProperty(object, property, properties[property]);
+            }
         }
         return object;
     };
