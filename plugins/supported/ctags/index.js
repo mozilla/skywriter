980e9bfc41f4e75235513e76a6b35a9e02f2ebf7
Patrick Walton <pwalton@mozilla.com>
2010-06-11 12:12:32 -0700

    Update ctags to revision 8bb4f452aaa4c8c8b4f1 (adds stemming support)

--- plugins/supported/ctags/index.js
+++ plugins/supported/ctags/index.js
@@ -48,6 +48,24 @@ exports.Tags = function() {
 };
 
 exports.Tags.prototype = Object.create(Object.prototype, Trait.compose(Trait({
+    _search: function(id, pred) {
+        var shadowTag = { name: id };
+        var tags = this.tags;
+        var index = _(tags).sortedIndex(shadowTag, function(tag) {
+            return tag.name;
+        });
+
+        var start = index, end = index;
+        while (start > 0 && start < tags.length && pred(tags[start])) {
+            start--;
+        }
+        while (end > 0 && end < tags.length && pred(tags[end])) {
+            end++;
+        }
+
+        return tags.slice(start + 1, end);
+    },
+
     add: function(src, file, opts) {
         if (opts === null || opts === undefined) {
             opts = {};
@@ -63,21 +81,15 @@ exports.Tags.prototype = Object.create(Object.prototype, Trait.compose(Trait({
 
     /** Returns all the tags that match the given identifier. */
     get: function(id) {
-        var shadowTag = { name: id };
-        var tags = this.tags;
-        var index = _(tags).sortedIndex(shadowTag, function(tag) {
-            return tag.name;
+        return this._search(id, function(tag) { return tag.name === id; });
+    },
+
+    /** Returns all the tags that begin with the given prefix. */
+    stem: function(prefix) {
+        var len = prefix.length;
+        return this._search(prefix, function(tag) {
+            return tag.name.substring(0, len) === prefix;
         });
-
-        var start = index, end = index;
-        while (start > 0 && start < tags.length && tags[start].name === id) {
-            start--;
-        }
-        while (end > 0 && end < tags.length && tags[end].name === id) {
-            end++;
-        }
-
-        return tags.slice(start + 1, end);
     }
 }), TagReader, TagWriter));
 
