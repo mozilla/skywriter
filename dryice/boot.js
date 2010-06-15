5842fabe7c4d80fcbe4131ac9e452ece23ac330a
Julian Viereck <jviereck@mozilla.com>
2010-06-15 15:43:56 +0200

    Implement bug 535819: BespinEmbedded does not attach correctly to textarea

--- dryice/boot.js
+++ dryice/boot.js
@@ -37,6 +37,27 @@
 
 // This module depends only on Tiki.
 
+(function() {
+/**
+ * Returns the CSS property of element.
+ *   1) If the CSS property is on the style object of the element, use it, OR
+ *   2) Compute the CSS property
+ *
+ * If the property can't get computed, is 'auto' or 'intrinsic', the former
+ * calculated property is uesd (this can happen in cases where the textarea
+ * is hidden and has no dimension styles).
+ */
+var getCSSProperty = function(element, container, property) {
+    var ret = element.style[property]
+                || document.defaultView.getComputedStyle(element, '').
+                                        getPropertyValue(property);
+
+    if (!ret || ret == 'auto' || ret == 'intrinsic') {
+        ret = container.style[property];
+    }
+    return ret;
+}
+
 bespin.useBespin = function(element, options) {
     var util = bespin.tiki.require('bespin:util/util');
 
@@ -46,7 +67,7 @@ bespin.useBespin = function(element, options) {
     for (var key in options) {
         baseConfig[key] = options[key];
     }
-    
+
     // we need to separately merge the configured settings
     var configSettings = baseConfig.settings;
     if (baseSettings !== undefined) {
@@ -58,24 +79,117 @@ bespin.useBespin = function(element, options) {
     }
 
     var Promise = bespin.tiki.require('bespin:promise').Promise;
+    var prEnv = null;
     var pr = new Promise();
 
     bespin.tiki.require.ensurePackage("::appconfig", function() {
         var appconfig = bespin.tiki.require("appconfig");
         if (util.isString(element)) {
-            baseConfig.element = document.getElementById(element);
+            element = document.getElementById(element);
+        }
+
+        if (util.none(baseConfig.initialContent)) {
+            baseConfig.initialContent = element.innerHTML;
+        }
+
+        element.innerHTML = '';
+
+        if (element.type == 'textarea') {
+            var parentNode = element.parentNode;
+            // This will hold the Bespin editor.
+            var container = document.createElement('div');
+
+            // To put Bespin in the place of the textarea, we have to copy a
+            // few of the textarea's style attributes to the div container.
+            //
+            // The problem is, that the properties have to get computed (they
+            // might be defined by a CSS file on the page - you can't access
+            // such rules that apply to an element via elm.style). Computed
+            // properties are converted to pixels although the dimension might
+            // be given as percentage. When the window resizes, the dimensions
+            // defined by percentages changes, so the properties have to get
+            // recomputed to get the new/true pixels.
+            var resizeEvent = function() {
+                var style = 'position:relative;';
+                [   'width', 'height',
+                    'margin-top', 'margin-left', 'margin-right', 'margin-bottom',
+                    'padding-top', 'padding-bottom', 'padding-left', 'padding-right'
+                ].forEach(function(item) {
+                    style += item + ':' +
+                                getCSSProperty(element, container, item) + ';';
+                });
+                container.setAttribute('style', style);
+            };
+            window.addEventListener('resize', resizeEvent, false);
+
+            // Call the resizeEvent once, so that the size of the container is
+            // calculated.
+            resizeEvent();
+
+            // Insert the div container after the element.
+            if (element.nextSibling) {
+                parentNode.insertBefore(container, element.nextSibling);
+            } else {
+                parentNode.appendChild(container);
+            }
+
+            // Define setter and getter on the textarea. Forwards set value
+            // and get value to the editor.
+            element.__defineGetter__('value', function() {
+                // If there is a prEnv (holds the env passed by the resolved
+                // promise), then access the editor directly, otherwise
+                // use the init value;
+                if (prEnv) {
+                    return prEnv.editor.value;
+                } else {
+                    return baseConfig.initialContent;
+                }
+            });
+            element.__defineSetter__('value', function(value) {
+                // If there is already an environment, ten set the value
+                // directly, otherwise set the value to the baseConfig obj
+                // and set the value after the app is launched.
+                if (prEnv) {
+                    prEnv.editor.value = value;
+                } else {
+                    baseConfig.initialContent = value;
+                    pr.then(function(env) {
+                        env.editor.value = value;
+                    });
+                }
+            });
+
+            // Override the forms onsubmit function. Set the innerHTML of the
+            // textarea before submitting.
+            while (parentNode.tagName != 'BODY') {
+                if (parentNode.tagName == 'FORM') {
+                    var oldSumit = parentNode.onsubmit;
+                    // Override the onsubmit function of the form.
+                    parentNode.onsubmit = function(evt) {
+                        element.innerHTML = element.value;
+                        // If there is a onsubmit function already, then call
+                        // it with the current context and pass the event.
+                        if (oldSumit) {
+                            oldSumit.call(this, evt);
+                        }
+                    }
+                    break;
+                }
+                parentNode = parentNode.parentNode;
+            }
+
+            // Hide the element.
+            element.style.display = 'none';
+
+            // The div container is the new element that is passed to appconfig.
+            baseConfig.element = container;
         } else {
             baseConfig.element = element;
         }
 
-        if (util.none(baseConfig.initialContent)) {
-            baseConfig.initialContent = baseConfig.element.innerHTML;
-        }
-
-        baseConfig.element.innerHTML = '';
-
-        appconfig.launch(baseConfig).then(function(result) {
-            pr.resolve(result);
+        appconfig.launch(baseConfig).then(function(env) {
+            prEnv = env;
+            pr.resolve(env);
         });
     });
 
@@ -89,7 +203,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var nodes = document.querySelectorAll(".bespin");
     for (var i = 0; i < nodes.length; i++) {
         var node = nodes[i];
-        var options = node.getAttribute('data-bespinoptions');
+        var options = node.getAttribute('data-bespinoptions') || '{}';
         var pr = bespin.useBespin(node, JSON.parse(options));
         pr.then(function(env) {
             node.bespin = env;
@@ -113,3 +227,5 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 }, false);
+
+})();
