d669d3b75322a125ca28a53f5c3f62082e9b42ea
Camilo Aguilar <camilo@cloudescape.com>
2010-09-22 13:27:15 -0400

    [dryice] assets needed in the package process and dryice config file

---
+++ dryice/lib/config.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var config = exports;
+
+var version = {	number: '0.9a2',
+				name: 'Edison',
+                api: 4
+			};
+
+var embedded = {	files: {
+						shared: 'SkywriterEmbedded.js', 
+						main: 'SkywriterMain.js',
+						worker: 'SkywriterWorker.js',
+						css: 'SkywriterEmbedded.css'
+					}, 
+					
+					boot: 'dryice/assets/boot.js',
+					preamble: 'dryice/assets/preamble.js',
+					script2loader: 'dryice/assets/script2loader.js',
+					
+					loader: 'dryice/thirdparty/tiki.js' 	
+				};
+								
+var plugins_path = {	supported: 'platform/browser/plugins/supported', 
+						thirdparty: 'platform/browser/plugins/thirdparty', 
+						labs: 'platform/browser/plugins/labs', 
+						boot: 'platform/browser/plugins/boot' 
+					};
+					
+var dependencies = {	jquery: {host: 'http://code.jquery.com', port: 80, uri: '/jquery-1.4.2.js'} };
+
+//config.buildDir = 'tmp'; //it's already specified in the manifest
+config.version = version;
+config.embedded = embedded;
+config.plugins_path = plugins_path;
+config.dependencies = dependencies;
+
+
